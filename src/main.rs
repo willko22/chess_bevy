8969dfaea12a562e876d52ab4e70a34f0d15cca0
willko22 <8willko@gmail.com>
2025-01-20 11:19:08 +0100

    basic app with printing

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+
+
+fn main() {
+    App::new()
+        // .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, print_persons)
+        .run();
+}
+
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Person {
+        name: "Alice".to_string(),
+        age: 30,
+    });
+    commands.spawn(Person {
+        name: "Bob".to_string(),
+        age: 25,
+    });
+}
+
+pub fn print_persons(query: Query<&Person>) {
+    println!("Name | Age");
+    for person in query.iter() {
+        println!("{} | {}", person.name, person.age);
+    }
+}
+
+#[derive(Component)]
+pub struct Person {
+    pub name: String,
+    pub age: u32,
+}
